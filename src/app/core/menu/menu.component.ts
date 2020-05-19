00e53d05d636e4a2abf7ea68eec72fe0e072ea49
Dennis Frey <dennismfrey@gmail.com>
2020-05-18 20:10:13 -0400

    Locked down user access

--- src/app/core/menu/menu.component.ts
+++ src/app/core/menu/menu.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'src/app/model/menu-item.class';
+import { User } from 'src/app/model/user.class';
+import { SystemService } from 'src/app/service/system.service';
 
 @Component({
   selector: 'app-menu',
@@ -8,10 +10,13 @@ import { MenuItem } from 'src/app/model/menu-item.class';
 })
 export class MenuComponent implements OnInit {
   menuItems: MenuItem[] = [];
+  loggedInUser: User = new User;
 
-  constructor() { }
+  constructor(private sysSvc: SystemService) { }
 
   ngOnInit(): void {
+    this.loggedInUser = this.sysSvc.loggedInUser;
+    if (this.loggedInUser.admin || this.loggedInUser.reviewer){
     this.menuItems = [
       new MenuItem("User", "/user/list", "User List"),
       new MenuItem("Vendor", "/vendor/list", "Vendor List"),
@@ -20,6 +25,16 @@ export class MenuComponent implements OnInit {
       new MenuItem("Review", "/request/review", "Request Review List"),
       new MenuItem("Login", "/user/login", "Login")
     ];
+  }
+  else {
+    this.menuItems = [
+      new MenuItem("User", "/user/list", "User List"),
+      new MenuItem("Vendor", "/vendor/list", "Vendor List"),
+      new MenuItem("Product", "/product/list", "Product List"),
+      new MenuItem("Request", "/request/list", "Request List"),
+      new MenuItem("Login", "/user/login", "Login")
+    ];
+  }
   }
 
 }
